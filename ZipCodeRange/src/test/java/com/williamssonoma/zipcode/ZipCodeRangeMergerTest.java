6bcb940caaeec98664e4949bf7b669588e458a18
asajjad1234 <asajjad786@yahoo.com>
2019-01-22 01:05:36 -0800

    ZipCodeRangeMergerTest
    ZipCodeRangeMergerTest

--- ZipCodeRange/src/test/java/com/williamssonoma/zipcode/ZipCodeRangeMergerTest.java
+++ ZipCodeRange/src/test/java/com/williamssonoma/zipcode/ZipCodeRangeMergerTest.java
@@ -30,9 +30,8 @@ public class ZipCodeRangeMergerTest {
 	public void afterEachTest() {
 	}
 
-	// Test cases from 01 - 08 are for invalid cases
 	@Test
-	public void testAlphaCharsZipCode() {
+	public void testValidateZipCodesAlphaChars() {
 		String inputZipCodes = "[ABCDE,95742] [94226,94669] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
@@ -40,7 +39,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testNonAlphaNumericZipCode() {
+	public void testValidateZipCodesNonAlphaNumeric() {
 		String inputZipCodes = "[!(@#*,95742] [94226,94669] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
@@ -48,7 +47,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testAlphaNumericZipCode() {
+	public void testValidateZipCodesAlphaNumeric() {
 		String inputZipCodes = "[AB123,95742] [94226,94669] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
@@ -56,7 +55,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testAlphaNonAlphaNumericZipCode() {
+	public void testValidateZipCodesAlphaNonAlphaNumeric() {
 		String inputZipCodes = "[394$$,95742] [94226,94669] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
@@ -64,7 +63,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testNegativeZipCode() {
+	public void testValidateZipCodesNegative() {
 		String inputZipCodes = "[-1029,95742] [94226,94669] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
@@ -72,7 +71,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testBlankStringZipCode() {
+	public void testValidateZipCodesBlankString1() {
 		String inputZipCodes = "[,95742] [94226,94669] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
@@ -80,7 +79,15 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testEmptyStringZipCode() {
+	public void testValidateZipCodesBlankString2() {
+		String inputZipCodes = "[\"\",95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
+		assertEquals(false, isValid);
+	}
+	
+	@Test
+	public void testValidateZipCodesEmptyString1() {
 		String inputZipCodes = "[         ,95742] [94226,94669] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
@@ -88,17 +95,121 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testLessThan5DigitZipCode() {
-		String inputZipCodes = "[1234,95742] [94226,94669] [94133,94133] [94200,94229]";
+	public void testValidateZipCodesEmptyString2() {
+		String inputZipCodes = "[\"      \",95742] [94226,94669] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
 		assertEquals(false, isValid);
 	}
 	
-	
-	// Test cases from 09 - 16 are for valid cases									 
 	@Test
-	public void testAllZeroDigitsZipCode() {
+	public void testValidateZipCodesLessThan5Digit() {
+		String inputZipCodes = "[1234,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
+		assertEquals(false, isValid);
+	}
+									 
+	@Test
+	public void testValidateZipCodesMoreThan5Digit() {
+		String inputZipCodes = "[123456,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		boolean isValid = zipCodeRangeMerger.validateZipCodes(zipCodeList);
+		assertEquals(false, isValid);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeNullZipRanges() {
+		List<ZipCodeRange> zipCodeRangeList = null;
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeEmptyZipRanges() {
+		List<ZipCodeRange> zipCodeRangeList = new ArrayList<ZipCodeRange>();
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeAlphaChars() {
+		String inputZipCodes = "[ABCDE,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeNonAlphaNumeric() {
+		String inputZipCodes = "[!(@#*,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeAlphaNumeric() {
+		String inputZipCodes = "[AB123,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeAlphaNonAlphaNumeric() {
+		String inputZipCodes = "[394$$,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeNegative() {
+		String inputZipCodes = "[-1029,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeBlankString1() {
+		String inputZipCodes = "[,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeBlankString2() {
+		String inputZipCodes = "[\"\",95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeEmptyString1() {
+		String inputZipCodes = "[         ,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeEmptyString2() {
+		String inputZipCodes = "[\"      \",95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeLessThan5Digit() {
+		String inputZipCodes = "[1234,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testMergeZipCodeRangeMoreThan5Digit() {
+		String inputZipCodes = "[123456,95742] [94226,94669] [94133,94133] [94200,94229]";
+		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
+		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
+	}
+	
+	@Test
+	public void testMergeZipCodeRangeAllZeroDigits() {
 		String inputZipCodes = "[00000,95742] [94226,94669] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
@@ -106,7 +217,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testDisjointIntervalZipCode() {
+	public void testMergeZipCodeRangeDisjointInterval() {
 		String inputZipCodes = "[94133,94149] [94200,94299] [94600,94699]";
 		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
@@ -114,8 +225,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testAdjacentIntervalZipCode() {
-		System.out.println ("testZipCodeRangeMerger6");
+	public void testMergeZipCodeRangeAdjacentInterval() {
 		String inputZipCodes = "[95742,95743] [94226,94669] [95744,95760] ";
 		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
@@ -123,7 +233,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testOverlappedIntervalZipCode1() {
+	public void testMergeZipCodeRangeOverlappedInterval1() {
 		String inputZipCodes = "[94133,94149] [94200,94299] [94226,94399]";
 		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
@@ -131,7 +241,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testOverlappedIntervalZipCode2() {
+	public void testMergeZipCodeRangeOverlappedInterval2() {
 		String inputZipCodes = "[95630,95742] [94226,94669] [95730, 99999] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
@@ -139,7 +249,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testEnclosedIntervalZipCode1() {
+	public void testMergeZipCodeRangeEnclosedInterval() {
 		String inputZipCodes = "[95630,95742] [94226,94669] [95700, 95740] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
@@ -147,7 +257,7 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testFullyEnclosedIntervalZipCode1() {
+	public void testMergeZipCodeRangeFullyEnclosedInterval1() {
 		String inputZipCodes = "[95630,95742] [94226,94669] [00000, 99999] [94133,94133] [94200,94229]";
 		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
@@ -155,16 +265,10 @@ public class ZipCodeRangeMergerTest {
 	}
 	
 	@Test
-	public void testFullyEnclosedIntervalZipCode2() {
+	public void testMergeZipCodeRangeFullyEnclosedInterval2() {
 		String inputZipCodes = "[95630,95742] [95742,95743] [95744,95744] [94226,94669] [94133,94133] [00000, 99999] [94200,94229] [01028,01034] [01007,01037] [01060,01065]";
 		List<ZipCodeRange> zipCodeRangeList = zipCodeRangeMerger.getInputZipCodes(inputZipCodes);
 		List<ZipCodeRange> zipCodeListOut = zipCodeRangeMerger.mergeZipCodeRange(zipCodeRangeList);
 		assertEquals(1, zipCodeListOut.size());
-	}
-
-	@Test(expected = IllegalArgumentException.class)
-	public void testNullZipRangesIllegalArgException() throws Exception {
-		System.out.println("Output of testNullZipRangesIllegalArgException");
-		assertEquals(1, zipCodeRangeMerger.mergeZipCodeRange(null));
-	}
+	}	
 }
